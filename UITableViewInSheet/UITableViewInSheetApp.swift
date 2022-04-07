adc7362c191e9d0b16215be37807079d3b8ca8db
Maksim Bezdrobnij <maksim.bezdrobnij@wallester.com>
2022-04-07 18:01:41 +0300

    Initial Commit

---
+++ UITableViewInSheet/UITableViewInSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITableViewInSheetApp.swift
+//  UITableViewInSheet
+//
+//  Created by Maksim Bezdrobnoi on 07.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct UITableViewInSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
